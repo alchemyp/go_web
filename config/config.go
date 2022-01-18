1f62af5c269e293558e917d46699d7ff9dfcdffe
HUAWEI <alchemy_info@aliyun.com>
2022-01-18 16:07:24 +0800

    主入口框架搭好

--- config/config.go
+++ config/config.go
@@ -1,3 +1,43 @@
 package config
 
-//此文件定义配置信息的映射
+var Conf = new(AppConfig)
+
+//App配置映射
+type AppConfig struct {
+	*LogConfig   `json:"log" ini:"log"`
+	*MysqlConfig `json:"mysql" ini:"mysql"`
+	*RedisConfig `json:"redis" ini:"redis"`
+}
+
+//Mysql配置映射
+type MysqlConfig struct {
+	Host         string `json:"host" ini:"host"`
+	Port         int    `json:"port" ini:"port"`
+	UserName     string `json:"username" ini:"username"`
+	DB           string `json:"db" ini:"db"`
+	PassWord     string `json:"password" ini:"password"`
+	MaxOpenConns int    `json:"max_open_conns" ini:"max_open_conns"`
+	MaxIdleConns int    `json:"max_idle_conns" ini:"max_idle_conns"`
+}
+
+//Redis配置映射
+type RedisConfig struct {
+	Host     string `json:"host" ini:"host"`
+	Port     string `json:"port" ini:"port"`
+	PassWord string `json:"password" ini:"password"`
+	DB       string `json:"db" ini:"db"`
+}
+
+//日志配置的映射
+type LogConfig struct {
+	Level      string `json:"level" ini:"level"`
+	FileName   string `json:"filename" ini:"filename"`
+	MaxSize    int    `json:"max_size" ini:"max_size"`
+	MaxAge     int    `json:"max_age" ini:"max_age"`
+	MaxBackups int    `json:"max_backups" ini:"max_backups"`
+}
+
+//定义加载文件，映射到结构体的函数
+func LoadFromFile(cfgConfig string) (err error) {
+	return ini.MapTo(Conf, cfgConfig)
+}
--- main.go
+++ main.go
@@ -1,13 +1,42 @@
 package main
 
+import (
+	"bluebell_backend/dao/redis"
+	"fmt"
+	"go_web/config"
+	"go_web/dao/mysql"
+	"go_web/logger"
+	"go_web/routers"
+)
+
 //此处是主入口
 func main() {
 
 	//加载配置文件
+	if err := config.LoadFromFile("./conf/web.ini"); err != nil {
+		fmt.Printf("load config file err:%v\n", err) //当加载配置文件失败的时候，直接打印错误，退出执行
+		return
+	} //当上述都没错的时候，才会继续往下执行
 
 	//加载logger日志
+	if err := logger.Init("config.Conf.LogConfig"); err != nil {
+		fmt.Printf("logger config file err:%v\n", err)
+		return
+	}
 
 	//连接数据库
+	//连接mysql
+	if err := mysql.Init("config.Conf.MysqlConfig"); err != nil {
+		fmt.Printf("connect mysql config err:%v\n", err)
+		return
+	}
+
+	//连接redis
+	if err := redis.Init("config.Conf.RedisConfig"); err != nil {
+		fmt.Printf("connect redis config err:%v\n", err)
+		return
+	}
 
 	//启动路由
+	routers.SetupRouter()
 }
